2c979d7d8aa94781ceb5f89c402f8063452d5ac8
Alejandro Pena-Bello <alefunxo@gmail.com>
2018-12-04 15:37:30 +0100

    egg

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 from setuptools import setup,find_packages
 
 
+	  
 
 	
 setup(
@@ -18,22 +19,11 @@ setup(
     description="BASOPRA - BAttery Schedule OPtimizer for Residential Applications",
     #long_description=read("README.txt"),
     packages=find_packages(exclude=['docs','tests*']),
-    install_requires=['numpy', 
-						'sys', 
-						'glob',
-						'os', 
-						'csv', 
-						'pickle', 
-						'functools', 
-						'argparse',
-						'itertools', 
-						'time', 
-						'math', 
-						'pyomo<=5.5', 
-						'multiprocessing'],
+    install_requires=['pip>=6.0',
+						'numpy>=1.15.1', 
+						'Pyomo==5.4.3'],
 
     classifiers=[
         'Development Status :: Alpha',
         'License :: OSI Approved :: GNU GPLv3',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6'])
+        'Programming Language :: Python'])
