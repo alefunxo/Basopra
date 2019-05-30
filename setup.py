4c89aebae9632a2de58be44df0fa0d82168dc1ac
alefunxo <alefunxo@gmail.com>
2019-05-30 16:27:38 +0200

    requirements mods

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=['pip>=6.0',
 			'pandas>=0.24.1',
 						'numpy>=1.15.1', 
-						'Pyomo==5.6',
+						'Pyomo==5.5',
 						'setuptools>=40.2.0'],
 
     classifiers=[
